d1342cdceba11d6d70d4737abbc938722414ed79
Денис Хрущев <squil@mail.ru>
2020-04-22 12:27:42 +0900

    Бинарный (логарифмический) поиск

--- finding/Form1.Designer.cs
+++ finding/Form1.Designer.cs
@@ -29,32 +29,129 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.edt_identificator = new System.Windows.Forms.TextBox();
+            this.btn_findIdentificator = new System.Windows.Forms.Button();
+            this.lbl_indIdIdentificator = new System.Windows.Forms.Label();
+            this.lbl_indIdIdentificator2 = new System.Windows.Forms.Label();
+            this.lbl_timer1 = new System.Windows.Forms.Label();
+            this.lbl_timer2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(550, 266);
+            this.button1.Location = new System.Drawing.Point(12, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(83, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Формируем список идентификаторов";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 368);
+            this.listBox1.TabIndex = 2;
+            // 
+            // edt_identificator
+            // 
+            this.edt_identificator.Location = new System.Drawing.Point(217, 74);
+            this.edt_identificator.Name = "edt_identificator";
+            this.edt_identificator.Size = new System.Drawing.Size(100, 20);
+            this.edt_identificator.TabIndex = 3;
+            // 
+            // btn_findIdentificator
+            // 
+            this.btn_findIdentificator.Location = new System.Drawing.Point(335, 74);
+            this.btn_findIdentificator.Name = "btn_findIdentificator";
+            this.btn_findIdentificator.Size = new System.Drawing.Size(75, 23);
+            this.btn_findIdentificator.TabIndex = 4;
+            this.btn_findIdentificator.Text = "Найти";
+            this.btn_findIdentificator.UseVisualStyleBackColor = true;
+            this.btn_findIdentificator.Click += new System.EventHandler(this.btn_findIdentificator_Click);
+            // 
+            // lbl_indIdIdentificator
+            // 
+            this.lbl_indIdIdentificator.AutoSize = true;
+            this.lbl_indIdIdentificator.Location = new System.Drawing.Point(430, 74);
+            this.lbl_indIdIdentificator.Name = "lbl_indIdIdentificator";
+            this.lbl_indIdIdentificator.Size = new System.Drawing.Size(35, 13);
+            this.lbl_indIdIdentificator.TabIndex = 5;
+            this.lbl_indIdIdentificator.Text = "label2";
+            // 
+            // lbl_indIdIdentificator2
+            // 
+            this.lbl_indIdIdentificator2.AutoSize = true;
+            this.lbl_indIdIdentificator2.Location = new System.Drawing.Point(430, 96);
+            this.lbl_indIdIdentificator2.Name = "lbl_indIdIdentificator2";
+            this.lbl_indIdIdentificator2.Size = new System.Drawing.Size(35, 13);
+            this.lbl_indIdIdentificator2.TabIndex = 6;
+            this.lbl_indIdIdentificator2.Text = "label2";
+            // 
+            // lbl_timer1
+            // 
+            this.lbl_timer1.AutoSize = true;
+            this.lbl_timer1.Location = new System.Drawing.Point(620, 74);
+            this.lbl_timer1.Name = "lbl_timer1";
+            this.lbl_timer1.Size = new System.Drawing.Size(35, 13);
+            this.lbl_timer1.TabIndex = 7;
+            this.lbl_timer1.Text = "label2";
+            // 
+            // lbl_timer2
+            // 
+            this.lbl_timer2.AutoSize = true;
+            this.lbl_timer2.Location = new System.Drawing.Point(620, 96);
+            this.lbl_timer2.Name = "lbl_timer2";
+            this.lbl_timer2.Size = new System.Drawing.Size(35, 13);
+            this.lbl_timer2.TabIndex = 8;
+            this.lbl_timer2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_timer2);
+            this.Controls.Add(this.lbl_timer1);
+            this.Controls.Add(this.lbl_indIdIdentificator2);
+            this.Controls.Add(this.lbl_indIdIdentificator);
+            this.Controls.Add(this.btn_findIdentificator);
+            this.Controls.Add(this.edt_identificator);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox edt_identificator;
+        private System.Windows.Forms.Button btn_findIdentificator;
+        private System.Windows.Forms.Label lbl_indIdIdentificator;
+        private System.Windows.Forms.Label lbl_indIdIdentificator2;
+        private System.Windows.Forms.Label lbl_timer1;
+        private System.Windows.Forms.Label lbl_timer2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- finding/Form1.cs
+++ finding/Form1.cs
@@ -7,14 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace finding
 {
     public partial class Form1 : Form
-    {
+    { 
+
+        List<int> lst_IdentificationLogarifmic = new List<int>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            int identificator;
+            // initialisation
+            Random rnd = new Random();
+            int count = 0;
+            lst_IdentificationLogarifmic.Clear();
+            for(int i = 1; i<10000; i++)
+            {
+                identificator = rnd.Next(1,10000);
+                if (0 == lst_IdentificationLogarifmic.Find(id => id == identificator))
+                {
+                    lst_IdentificationLogarifmic.Add(identificator);
+                    count++;
+                }                
+            }
+            label1.Text = count.ToString();
+            //listBox1.;
+            lst_IdentificationLogarifmic.Sort();
+            listBox1.DataSource = lst_IdentificationLogarifmic.ToArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_findIdentificator_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            //system method
+            stopWatch.Start();
+            lbl_indIdIdentificator.Text = findSystemStandart(lst_IdentificationLogarifmic, int.Parse(edt_identificator.Text));
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            lbl_timer1.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",ts.Hours, ts.Minutes, ts.Seconds,ts.Milliseconds / 10);
+
+            //logarifmic method
+            stopWatch.Start();
+            lbl_indIdIdentificator2.Text = findLogarifmic(lst_IdentificationLogarifmic, int.Parse(edt_identificator.Text));
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            lbl_timer2.Text = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+        }
+
+        private string findLogarifmic(List<int> list, int value)
+        {
+            int move = 0; 
+            int finish = list.Count;
+            int iteration = 1;
+            int n = (finish + 1) / 2; 
+            while(list[move + n - 1] != value)
+            {
+                if (list[move + n - 1] < value)
+                {
+                    move = n-1;
+                }
+                else
+                {
+                    finish = n+1;
+                }
+                n = (finish - move + 1) / 2;
+                iteration++;
+            }
+            return String.Format("result - {0}, itarations - {1}", 
+                (list[move + n - 1]).ToString(),
+                iteration);
+        }
+        private string findSystemStandart(List<int> list, int value)
+        {
+            return (list.FindIndex(x => x == value)).ToString();
+        }
+    
     }
 }
+ 
